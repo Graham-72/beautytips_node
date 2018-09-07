25c274dedde3c9ece453d40ebc257ba1e28a9e0b
Graham-72 <g.oliver@btinternet.com>
2018-09-07 17:24:20 +0100

    Fix issue #4 with revised script.

--- js/beautytips_node.js
+++ js/beautytips_node.js
@@ -12,33 +12,19 @@
       var defaultStyleSettings = styles[defaultStyle];
 
       $('.beautytips-node:not(.beautytips-processed)').addClass('beautytips-processed').each(function() {
-        var wrapper = $(this);
-        $(this).find('a').each(function() {
-          defaultStyleSettings.trigger = 'none';
-          defaultStyleSettings.ajaxPath = '/beautytips' + $(this).attr('href');
-          $(wrapper).bt(defaultStyleSettings);
-        });
-        $(wrapper).click(function() {
-          // $(this).btOn();
-          // return false;
-        });
-        // var triggerOn = beautytipsnode.triggerOn;
-        // var triggerOff = beautytipsnode.triggerOff;
-        // $(wrapper).triggerOn(function(){
-        //   $(this).btOn();
-        // });
-        // $(wrapper).triggerOff(function(){
-        //   $(this).btOff();
-        // });
-        $(wrapper).hover(
+        defaultStyleSettings.trigger = 'none';
+        defaultStyleSettings.ajaxPath = '/beautytips' + $(this).attr('href');
+        $(this).bt(defaultStyleSettings);
+        $(this).hover(
           function() {
             $(this).btOn();
             return false;
-          }, function() {
-            // $(this).btOff();
-            // return false;
+          },
+          function() {
+            $(this).btOff();
+            return false;
           }
-        );
+        )
       });
     }
   };
